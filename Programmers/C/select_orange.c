5ee27f97fb712bab124bfe5c856533f64395f33f
Lee Keyom <121466350+LeeKeyom@users.noreply.github.com>
2023-05-09 16:06:24 +0900

    Create select_orange.c

---
+++ Programmers/C/select_orange.c
@@ -0,0 +1,30 @@
+
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+
+int Compare(const void* first, const void* second){
+    return *(int*)second - *(int*)first;
+}
+
+// tangerine_len은 배열 tangerine의 길이입니다.
+int solution(int k, int tangerine[], size_t tangerine_len) {
+    int answer = 0;
+    int *check = (int*)malloc(sizeof(int) * (tangerine_len + 1));
+    memset(check, 0, sizeof(int) * (tangerine_len + 1));
+
+    qsort(tangerine, tangerine_len, sizeof(int), Compare);
+    check[0]++;
+    for(int i = 0, j = 0; i < (int)tangerine_len; i++){
+        if(tangerine[i] != tangerine[i - 1]) j++;
+        check[j]++;
+    }
+    qsort(check, tangerine_len + 1, sizeof(int), Compare);
+    for(int i = 0, sum = 0; i < (int)tangerine_len + 1; i++){
+        sum += check[i];
+        answer++;
+        if(sum > k - 1) break;
+    }
+    return answer;
+}
